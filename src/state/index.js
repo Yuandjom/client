82ef0c1cdc4368ca1663b14d6fec87efb5c09e99
Yuandjom <ziyangjohn@gmail.com>
2023-01-04 20:30:32 +0800

    test

---
+++ src/state/index.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  mode: "dark",
+};
+
+export const globalSlice = createSlice({
+  name: "global",
+  initialState,
+  //basically think of reducers as functions that changes the global state
+  reducers: {
+    setMode: (state) => {
+      state.mode = state.mode === "light" ? "dark" : "light";
+    },
+  },
+});
+
+export const { setMode } = globalSlice.actions;
+
+export default globalSlice.reducer;
